3050934861fea5becd4199664231ccfa1ad0dbf4
TUANANH <ledinhtuananh200103@gmail.com>
2023-10-01 10:58:21 +0700

    Create dai.py

---
+++ dai.py
@@ -0,0 +1,5 @@
+day_lon = float(input("Nhập chiều dài đáy lớn: "))
+day_nho = float(input("Nhập chiều dài đáy nhỏ: "))
+chieu_cao = float(input("Nhập chiều cao: "))
+dien_tich = (day_lon + day_nho) * chieu_cao / 2
+print("Diện tích của hình thang là:", dien_tich)
